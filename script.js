13e9e5f17cd573f21149580515b7d7287fc3cc5f
lizandreina <156568298+lizandreina@users.noreply.github.com>
2024-06-15 00:41:58 +0200

    Initiate a round by pressing the 'Rock' button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getComputerChoice(result) {
+function getComputerChoice() {
     result = Math.round(Math.random() * (2 - 0) + 0);
     if (result === 0) {
         return 'rock';
@@ -9,9 +9,9 @@ function getComputerChoice(result) {
     }
 }
 
-function getHumanChoice() {
-    return prompt("Your turn!", "Rock, Paper, or Scissors");
-}
+let humanRock = document.querySelector('.rock');
+let humanPaper = document.querySelector('.paper');
+let humanScissors = document.querySelector('.scissors');
 
 function playGame() {
     let humanScore = 0;
@@ -45,36 +45,44 @@ function playGame() {
         }
     }
 
-    // Round 1
-    let computerAnswerOne = getComputerChoice();
-    let humanAnswerOne = getHumanChoice().toLowerCase();
-    playRound(computerAnswerOne, humanAnswerOne);
-    console.log(computerScore);
-    console.log(humanScore);
-    // Round 2
-    let computerAnswerTwo = getComputerChoice();
-    let humanAnswerTwo = getHumanChoice().toLowerCase();
-    playRound(computerAnswerTwo, humanAnswerTwo);
-    console.log(computerScore);
-    console.log(humanScore);
-    // Round 3
-    let computerAnswerThree = getComputerChoice();
-    let humanAnswerThree = getHumanChoice().toLowerCase();
-    playRound(computerAnswerThree, humanAnswerThree);
-    console.log(computerScore);
-    console.log(humanScore);
-    // Round 4
-    let computerAnswerFour = getComputerChoice();
-    let humanAnswerFour = getHumanChoice().toLowerCase();
-    playRound(computerAnswerFour, humanAnswerFour);
-    console.log(computerScore);
-    console.log(humanScore);
-    // Round 5
-    let computerAnswerFive = getComputerChoice();
-    let humanAnswerFive = getHumanChoice().toLowerCase();
-    playRound(computerAnswerFive, humanAnswerFive);
-    console.log(computerScore);
-    console.log(humanScore);
+    humanRock.addEventListener('click', function() {
+        playRound(getComputerChoice, 'rock');
+        return console.log(`Your score is ${humanScore} and the computer's score is ${computerScore}`);
+    });
+
+    humanPaper.addEventListener('click', ()=>playRound(getComputerChoice, 'paper'));
+    humanScissors.addEventListener('click', ()=>playRound(getComputerChoice, 'scissors'));
+    
+    // // Round 1
+    // let computerAnswerOne = getComputerChoice();
+    // let humanAnswerOne = getHumanChoice().toLowerCase();
+    // playRound(computerAnswerOne, humanAnswerOne);
+    // console.log(computerScore);
+    // console.log(humanScore);
+    // // Round 2
+    // let computerAnswerTwo = getComputerChoice();
+    // let humanAnswerTwo = getHumanChoice().toLowerCase();
+    // playRound(computerAnswerTwo, humanAnswerTwo);
+    // console.log(computerScore);
+    // console.log(humanScore);
+    // // Round 3
+    // let computerAnswerThree = getComputerChoice();
+    // let humanAnswerThree = getHumanChoice().toLowerCase();
+    // playRound(computerAnswerThree, humanAnswerThree);
+    // console.log(computerScore);
+    // console.log(humanScore);
+    // // Round 4
+    // let computerAnswerFour = getComputerChoice();
+    // let humanAnswerFour = getHumanChoice().toLowerCase();
+    // playRound(computerAnswerFour, humanAnswerFour);
+    // console.log(computerScore);
+    // console.log(humanScore);
+    // // Round 5
+    // let computerAnswerFive = getComputerChoice();
+    // let humanAnswerFive = getHumanChoice().toLowerCase();
+    // playRound(computerAnswerFive, humanAnswerFive);
+    // console.log(computerScore);
+    // console.log(humanScore);
 
     if (humanScore>computerScore) {
         console.log('Congrats, you win the game!')
